fd699630a4dce8e77a56d358fbd8d25de42d87e5
Atharv Bharadwaj <96018989+0019atharv@users.noreply.github.com>
2025-08-06 11:40:24 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,11 +1,11 @@
 import os
 from sqlalchemy import create_engine
-
-DB_URL = os.getenv('DB_URL')  # Fetch from environment variable
+DB_URL = os.getenv('CLOUD_URL_OF_DB')
 
 def get_db_connection():
     try:
         engine = create_engine(DB_URL)
+        print("Engine",engine)
         return engine
     except Exception as e:
         print(f"Error: {e}")
